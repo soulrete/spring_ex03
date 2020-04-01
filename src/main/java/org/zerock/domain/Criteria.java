14410d313a9844a32df477392b03ff1f84cf971b
telestop <telestop@DESKTOP-FA6BLMD>
2020-04-02 02:38:41 +0900

    코드로 배우는 스프링 PART3 완료

---
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,43 @@
+package org.zerock.domain;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+	
+	private int pageNum;
+	private int amount;
+	
+	private String type;
+	private String keyword;
+	
+	public Criteria() {
+		this(1,10);
+	}
+	
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount; 
+	}
+	
+	public String[] getTypeArr() {
+		//split 구분자가 빈문자열인 경우 문자열 각각의 문자가 배열의 원소로 분리된다.
+		return type == null? new String[] {} : type.split(""); 
+	}
+	
+	public String getListLink() {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+				.queryParam("pageNum", this.pageNum)
+				.queryParam("amount", this.getAmount())
+				.queryParam("type", this.getType())
+				.queryParam("keyword", this.getKeyword());
+		
+		return builder.toUriString();
+	}
+}
